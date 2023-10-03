6e1e9af6aa807d5eeeb49f25466dc8f40d2f2fcf
rob <collinsduo1@gmail.com>
2023-10-03 14:32:17 -0500

    add src

---
+++ README.md
@@ -0,0 +1,17 @@
+# React-portfolio
+A React-based, straightforward SPA Portfolio template for designers and developers.Make use of it to showcase your work, client testimonials, and other data.
+
+## Preview
+
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` yarn add ``` or ``` npm install ```.
+3. Change the values in ```src/app.js``` to suit your use-case.
+4. Run ```yarn start``` or ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/app.js``` to suit your needs.
+
+Deploy-  
+Repo-
+
+
---
+++ src/app.js
@@ -0,0 +1,83 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <nav className="navbar"> 
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#AboutMe">About Me</a></li>
+        <li><a href="#Work">Work</a></li>
+        <li><a href="#ContactMe">Contact Me</a></li>
+      </ul>
+    </nav>
+   
+    <section id="Home">
+      <div Name="main"/> 
+        <h1 Name="Headings">LaTavya Collins </h1>
+    </section>
+   
+    <section id="AboutMe">
+      <h1 Name="headings">About Me</h1>
+      <div id="picture">
+        <img src="assets/Images/profile picture.jpg" alt="Picture of myself"/>
+        <div id="Introduction">
+          <p>
+            University of Texas Full Stack Coding Bootcamp 
+            Trainee and
+            Transitioning Science Educator. 
+            Originally
+             from Houston, I graduated
+            with a undergraduate degree 
+            in Psychology with a minor
+             in Human
+            Science from Prairie View A&M University.
+             I also have a Masters  in
+            Secondary Education from Texas State
+             University. In my free time, I
+            enjoy hanging out with my family, 
+            friends and our dog, I love trying
+            new brunch places and watching Tv shows.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section id="Work"/>
+      <h1 Name="headings">Work</h1>
+      <div Name="projects"/>
+        <button Name="btn"/>
+        <h2> 
+          <a href="https://www.youtube.com" target="_blank">
+            <img src="./assets/Images/img5.jpg" alt="beach picture"  width="728"height="1740"> 
+           </img> </a>
+           </h2>
+    
+        <a href="https://www.w3schools.com/sql/default.asp" target="blank"/> 
+        <img src="./assets/Images/img2.jpg" alt="beach picture"/>
+
+          <a href="https://youtu.be/dQw4w9WgXcQm" target="_blank"/>
+          <img src="./assets/Images/img3.jpg" alt="beach picture"/>
+
+          <a href="https://www.Github.com" target="_blank"/>
+          <img src="./assets/Images/img6.jpg" alt="beach picture"/>
+
+          <a href="https://www.google.com" target="_blank"/>
+          <img src="./assets/Images/img4.jpg" alt="beach picture"/>
+        <h3/>
+        <button/>
+      <div/>
+
+   
+    <section id="ContactMe"/>
+      <h1 Name="headings">Contact Me</h1>
+      <p>111-111-111</p>
+      <p>LaTavya-Collins@website</p>
+    </div>
+    
+  );
+
+  
+  }export default App;
+
+  
