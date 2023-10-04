6fd010fccbb290cfa856c826062c7023ebdfcaec
rob <collinsduo1@gmail.com>
2023-10-04 16:45:43 -0500

    add nav and project tab

--- src/components/header/cta.jsx
+++ src/components/header/cta.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import resume from '../../assets/resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
+       <a href={resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn'>Contact</a>
     </div>
   )
